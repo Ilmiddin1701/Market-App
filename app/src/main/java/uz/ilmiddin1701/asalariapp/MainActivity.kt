4d63b99f6e198798271055e53feb9775e7a3f713
Ilmiddin O'rinov <ilmiddinorino1@gmail.com>
2024-07-19 09:18:01 -0700

    asalari app

--- app/src/main/java/uz/ilmiddin1701/asalariapp/MainActivity.kt
+++ app/src/main/java/uz/ilmiddin1701/asalariapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -26,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
         MyData.permissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
